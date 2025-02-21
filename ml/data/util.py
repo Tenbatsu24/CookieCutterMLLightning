c8ce249170107640a091844ceb84fc8894a7a0f7
Tenbatsu24 <puruvaish24@outlook.com>
2025-02-22 00:58:54 +0100

    change: singleton register and central store setup

--- ml/data/util.py
+++ ml/data/util.py
@@ -1,26 +1,13 @@
-from ml.util import registry
+from loguru import logger
+
+from ml.util import STORE
+
+TYPE_OF: str = "data"
 
 
-def register(type_of, name):
-    """
-    Register a class with the registry
-    :param type_of: the top level type of the class. it will be the first key in the registry
-    :param name: the name of the class. it will be the second key in the registry
-    :return: the class
-    """
-
-    def inner(cls):
-        registry.register(type_of, name, cls)
-        return cls
-
-    return inner
+@STORE.reg(TYPE_OF, "test")
+class Test:
+    pass
 
 
-def get_class(type_of, name):
-    """
-    Get a class from the registry
-    :param type_of: the top level type of the class
-    :param name: the name of the class
-    :return: the class
-    """
-    return registry.get_class(type_of, name)
+logger.info(STORE)
--- ml/util.py
+++ ml/util.py
@@ -7,45 +7,57 @@ class SingletonRegistry:
             cls._instance = super(SingletonRegistry, cls).__new__(cls)
         return cls._instance
 
-    def register(self, type_of, name, cls):
-        if type_of not in self._registry:
-            self._registry[type_of] = {}
-        self._registry[type_of][name] = cls
+    def register(self, name, cls):
+        self._registry[name] = cls
 
-    def get_class(self, type_of, name):
-        if type_of not in self._registry:
-            raise ValueError(f"Unknown type {type_of}")
-        if name not in self._registry[type_of]:
-            raise ValueError(f"Unknown {type_of} {name}")
-        return self._registry[type_of][name]
+    def get_class(self, name):
+        if name not in self._registry:
+            raise ValueError(f"Unknown {name}")
+        return self._registry[name]
 
     def get_registry(self):
         return self._registry
 
-
-registry = SingletonRegistry()
-
-
-def register(type_of, name):
-    """
-    Register a class with the registry
-    :param type_of: the top level type of the class. it will be the first key in the registry
-    :param name: the name of the class. it will be the second key in the registry
-    :return: the class
-    """
-
-    def inner(cls):
-        registry.register(type_of, name, cls)
-        return cls
-
-    return inner
+    def __str__(self):
+        return f"\n".join([f"    {k}: {v}" for k, v in self._registry.items()])
 
 
-def get_class(type_of, name):
-    """
-    Get a class from the registry
-    :param type_of: the top level type of the class
-    :param name: the name of the class
-    :return: class
-    """
-    return registry.get_class(type_of, name)
+class RegistryStore:
+    def __init__(self):
+        self._instances = {}
+
+    def get_instance(self, type_of):
+        if type_of not in self._instances:
+            self._instances[type_of] = SingletonRegistry()
+        return self._instances[type_of]
+
+    def reg(self, type_of, name):
+        """
+        Register a class with the registry
+        :param type_of: the top level type of the class. it will be the first key in the registry
+        :param name: the name of the class. it will be the second key in the registry
+        :return: the class
+        """
+
+        def inner(cls):
+            register = self.get_instance(type_of)
+            register.register(name, cls)
+            return cls
+
+        return inner
+
+    def get(self, type_of, name):
+        """
+        Get a class from the registry
+        :param type_of: the top level type of the class
+        :param name: the name of the class
+        :return: class
+        """
+        register = self.get_instance(type_of)
+        return register.get_class(name)
+
+    def __str__(self):
+        return f"\nWarehouse:\n" + "\n".join([f"  {k}: {v}" for k, v in self._instances.items()])
+
+
+STORE = RegistryStore()
