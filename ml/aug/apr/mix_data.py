70118479d664dcde722c444887472b9bd834d3b5
Tenbatsu24 <puruvaish24@outlook.com>
2025-02-28 16:26:44 +0100

    change: remove unused variables

--- ml/aug/apr/mix_data.py
+++ ml/aug/apr/mix_data.py
@@ -30,10 +30,10 @@ def mix_data(x, prob=0.6):
     index = torch.randperm(batch_size, device=x.device)
 
     fft_1 = torch.fft.fftn(x, dim=(1, 2, 3))
-    abs_1, angle_1 = torch.abs(fft_1), torch.angle(fft_1)
+    angle_1 = torch.angle(fft_1)
 
     fft_2 = torch.fft.fftn(x[index, :], dim=(1, 2, 3))
-    abs_2, angle_2 = torch.abs(fft_2), torch.angle(fft_2)
+    abs_2 = torch.abs(fft_2)
 
     fft_1 = abs_2 * torch.exp(1j * angle_1)
 
