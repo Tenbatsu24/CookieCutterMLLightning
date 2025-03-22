6508487f21d12eca377157e1e2dd84a878c33902
Tenbatsu24 <puruvaish24@outlook.com>
2025-03-22 22:12:28 +0100

    change: register torchvision datasets

--- ml/data/__init__.py
+++ ml/data/__init__.py
@@ -1,8 +1,9 @@
-from ml.util import STORE
+from torchvision.datasets import MNIST, FashionMNIST, CIFAR10, CIFAR100, Imagenette
 
-TYPE_OF: str = "data"
+from ml.util import STORE, DATA_TYPE
 
-
-@STORE.reg(TYPE_OF, "test")
-class Test:
-    pass
+STORE.register(DATA_TYPE, "mnist", MNIST)
+STORE.register(DATA_TYPE, "fmnist", FashionMNIST)
+STORE.register(DATA_TYPE, "c10", CIFAR10)
+STORE.register(DATA_TYPE, "c100", CIFAR100)
+STORE.register(DATA_TYPE, "imagenette", Imagenette)
