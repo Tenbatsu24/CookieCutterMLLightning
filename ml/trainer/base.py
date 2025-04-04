fix: SAM gradient clipping

ml/trainer/base.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional, Union, Sequence
+from typing import Union, Sequence
 
 import torch
 import torch.nn as nn
@@ -24,9 +24,6 @@ class BaseTrainer(pl.LightningModule):
 
     def forward(self, x):
         return self.module(x)
-
-    def on_train_batch_start(self, batch: Any, batch_idx: int) -> Optional[int]:
-        self.module.step_scheduler()
 
     def training_step(self, batch, batch_idx):
         loss, metrics_dict = self.module.train_step(batch, batch_idx)
@@ -74,19 +71,17 @@ class SAMTrainer(BaseTrainer):
             m.train()
 
     def training_step(self, batch, batch_idx):
-        optimizers = self.optimizers()
+        optimizer = self.optimizers()
 
-        if not isinstance(optimizers, list):
-            optimizers = [optimizers]
-
-        self.module.apply(self.set_bn_eval)
+        self.module.apply(self.set_bn_train)
         loss_1, _ = self.module.train_step(batch, batch_idx)
 
         self.manual_backward(loss_1)
-        [optimizer.first_step(zero_grad=True) for optimizer in optimizers]
+        self.clip_gradients(optimizer, gradient_clip_val=0.5, gradient_clip_algorithm="norm")
+        optimizer.first_step(zero_grad=True)
 
         # second forward-backward pass
-        self.module.apply(self.set_bn_train)
+        self.module.apply(self.set_bn_eval)
         loss_2, metrics_dict = self.module.train_step(batch, batch_idx)
 
         self.log("train/loss", loss_2, prog_bar=True, on_epoch=False, on_step=True)
@@ -102,7 +97,8 @@ class SAMTrainer(BaseTrainer):
         self.trainer.fit_loop.epoch_loop.manual_optimization._on_before_step()
 
         self.manual_backward(loss_2)
-        [optimizer.second_step(zero_grad=True) for optimizer in optimizers]
+        self.clip_gradients(optimizer, gradient_clip_val=0.5, gradient_clip_algorithm="norm")
+        optimizer.second_step(zero_grad=True)
 
         self.trainer.fit_loop.epoch_loop.manual_optimization._on_after_step()
 