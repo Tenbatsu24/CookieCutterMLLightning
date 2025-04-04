e5925b75052f748b1e4a24f488982b0caf439804
Tenbatsu24 <puruvaish24@outlook.com>
2025-04-04 17:56:24 +0200

    change: names of the datasets

--- ml/data/__init__.py
+++ ml/data/__init__.py
@@ -2,8 +2,8 @@ from torchvision.datasets import MNIST, FashionMNIST, CIFAR10, CIFAR100, Imagene
 
 from ml.util import STORE, DATA_TYPE
 
-STORE.register(DATA_TYPE, "mnist", MNIST)
-STORE.register(DATA_TYPE, "fmnist", FashionMNIST)
+STORE.register(DATA_TYPE, "m", MNIST)
+STORE.register(DATA_TYPE, "fm", FashionMNIST)
 STORE.register(DATA_TYPE, "c10", CIFAR10)
 STORE.register(DATA_TYPE, "c100", CIFAR100)
-STORE.register(DATA_TYPE, "imagenette", Imagenette)
+STORE.register(DATA_TYPE, "im10", Imagenette)
