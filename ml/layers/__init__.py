002351e2a9389ee2ca68e5d036f3b875e3c450b7
Tenbatsu24 <puruvaish24@outlook.com>
2025-04-08 16:39:59 +0200

    add: compact transformers

---
+++ ml/layers/__init__.py
@@ -0,0 +1,16 @@
+from .dino_head import DINOHead
+from .mlp import Mlp
+from .patch_embed import PatchEmbed
+from .swiglu_ffn import SwiGLUFFN, SwiGLUFFNFused
+from .block import NestedTensorBlock
+from .attention import MemEffAttention
+
+__all__ = [
+    "DINOHead",
+    "Mlp",
+    "PatchEmbed",
+    "SwiGLUFFN",
+    "SwiGLUFFNFused",
+    "NestedTensorBlock",
+    "MemEffAttention",
+]
--- ml/model/image/__init__.py
+++ ml/model/image/__init__.py
@@ -1,8 +1,28 @@
+from .compact_transformers import (
+    cct_7_3x1_32,
+    cct_14_7x2_224,
+    cvt_7_4_32,
+    cvt_14_16_224,
+    vit_lite_7_4_32,
+)
 from .cifar import ResNet18, ResNeXt29
 
 from ml.util import MODEL_TYPE, STORE
 
 STORE.register(MODEL_TYPE, "rn18", ResNet18)
 STORE.register(MODEL_TYPE, "rnxt29", ResNeXt29)
+STORE.register(MODEL_TYPE, "32-cct", cct_7_3x1_32)
+STORE.register(MODEL_TYPE, "224-cct", cct_14_7x2_224)
+STORE.register(MODEL_TYPE, "32-cvt", cvt_7_4_32)
+STORE.register(MODEL_TYPE, "224-cvt", cvt_14_16_224)
+STORE.register(MODEL_TYPE, "32-vit-lite", vit_lite_7_4_32)
 
-__all__ = ["ResNet18", "ResNeXt29"]
+__all__ = [
+    "ResNet18",
+    "ResNeXt29",
+    "cct_7_3x1_32",
+    "cct_14_7x2_224",
+    "cvt_7_4_32",
+    "cvt_14_16_224",
+    "vit_lite_7_4_32",
+]
