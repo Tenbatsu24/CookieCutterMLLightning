9c31a45751a1aa6a0c3242a3b1e23aefbd6d56fd
Tenbatsu24 <puruvaish24@outlook.com>
2025-06-04 23:56:26 +0200

    change: error message

--- ml/loss/__init__.py
+++ ml/loss/__init__.py
@@ -5,4 +5,4 @@ def get_loss(loss_name):
     if hasattr(torch.nn, loss_name):
         return getattr(torch.nn, loss_name)
     else:
-        raise ValueError(f"Loss {loss_name} not found in torch.nn or smp.losses")
+        raise ValueError(f"Loss {loss_name} not found in torch.nn")
