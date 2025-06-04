f0e609a686f9359d15b46f68550cb3f13ba4ec94
Tenbatsu24 <puruvaish24@outlook.com>
2025-06-05 00:54:43 +0200

    change: add trivial augment to init

--- ml/aug/__init__.py
+++ ml/aug/__init__.py
@@ -1,17 +1,21 @@
 from .afa import OrigAFA, RealNDimFourier, ScaledNDimFourierAFA
 from .apr import APR
-from .augmix import AugMix
+from .augmix import GPUAugMix
 from .prime import GeneralizedPRIMEModule, PRIMEAugModule, Diffeo, RandomSmoothColor, RandomFilter
+from .ta import TrivialAugment, TrivialAugmentWide, TrivialMix
 
 __all__ = [
     "OrigAFA",
     "RealNDimFourier",
     "ScaledNDimFourierAFA",
     "APR",
-    "AugMix",
+    "GPUAugMix",
     "GeneralizedPRIMEModule",
     "PRIMEAugModule",
     "Diffeo",
     "RandomSmoothColor",
     "RandomFilter",
+    "TrivialAugment",
+    "TrivialAugmentWide",
+    "TrivialMix",
 ]
